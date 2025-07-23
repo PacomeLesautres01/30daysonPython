be5103feccfdf09e8efb5b7d9e331494add96398
Nyx <nyxbc.01@gmail.com>
2025-07-23 23:31:37 +0000

    Premier jour du challenge Python

---
+++ helloworld.py
@@ -0,0 +1,58 @@
+#Exercices - Jour 1
+
+####### Exercice: niveau 2 #######
+
+# 1 : afficher la version de python
+# version = python --version
+# print(version)
+
+# 2 : afficher le résultat de quelques opérations arithmétiques
+print(3 + 4)        # addition(+)      
+print(3 - 4)        # soustraction(-)     
+print(3 * 4)        # multiplication(*)      
+print(3 / 4)        # division(/)      
+print(3 ** 4)       # exponentiel(**)      
+print(3 % 4)        # modulo(%)      
+print(3 // 4)       # division entière(//)     
+
+# 3 : afficher une chaîne de caractères
+print('My name is Pacôme')  
+print('My family name is NYABO') 
+print('I come from TOGO') 
+print('I am enjoying 30 days of python') 
+
+# 4 : afficher le type de données d'une variable
+print(type(10))          
+print(type(3.14))
+print(type(9.8))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Pacôme'))
+print(type('NYABO'))
+print(type('Togo'))
+
+# 5 : Exemples de différents types de données en Python 
+entier = 42                      # int
+flottant = 3.14                  # float
+complexe = 2 + 3j                # nombre complexe
+chaine = "Bonjour"               # String
+vrai_ou_faux = True              # Booleen
+ma_liste = [1, 2, 3, "texte", 4.5]    # Liste 
+mon_tuple = (10, 20, 30)         # Tuple
+mon_ensemble = {1, 2, 3, 3, 4}   # Ensemble
+mon_dictionnaire = {"nom": "Alice", "age": 25, "ville": "Paris"}  # Dictionnaire
+
+# Affichage
+print("Entier:", entier)
+print("Flottant:", flottant)
+print("Complexe:", complexe)
+print("Chaîne:", chaine)
+print("Booléen:", vrai_ou_faux)
+print("Liste:", ma_liste)
+print("Tuple:", mon_tuple)
+print("Ensemble:", mon_ensemble)
+print("Dictionnaire:", mon_dictionnaire)
+
+
+# 6 : Distance euclidienne
+
